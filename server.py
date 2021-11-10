db72c04d0e7e326496991e291bc805ad66121aa5
Itamar Gefen <itamargefen1@gmail.com>
2021-11-10 15:23:18 +0200

    now reciving port # from sys.argv, added try-except

--- server.py
+++ server.py
@@ -1,7 +1,31 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', 12345))
-while True:
-    data, address = s.recvfrom(1024)
-    print(str(data))
-    s.sendto(data, address)
+import sys
+
+BUFFER_LEN = 127
+
+def main():
+    try:
+        PORT = int(sys.argv[1]) # argv[1] is our port
+    except:
+        print("invalid port")
+        return
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.bind(('', PORT))
+    except:
+        print("error binding to port")
+        return
+    
+    while True:
+        data, address = s.recvfrom(BUFFER_LEN)
+        print(str(data))
+        try:
+            s.sendto(data, address)
+        except:
+            print("error sending")
+            return
+# end of main
+
+if __name__ == "__main__":
+    main()
