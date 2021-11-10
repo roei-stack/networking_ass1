9c33cc7711169202c2cf26eaddb9885241d9c41c
Itamar Gefen <itamargefen1@gmail.com>
2021-11-10 18:20:29 +0200

    added try-except

--- level2/client2.py
+++ level2/client2.py
@@ -6,16 +6,29 @@ MAX_FILE_SIZE = 50000
 MSS = 100
 
 SERVER_IP = sys.argv[1]
-SERVER_PORT = int(sys.argv[2])
+SERVER_PORT = 0
+try:
+    SERVER_PORT = int(sys.argv[2])
+except:
+    print("invalid port number")
+    exit(0)
 ADDRESS = (SERVER_IP, SERVER_PORT)
 FILE_NAME = sys.argv[3]
+# making sure the file name given exists
+if not os.path.isfile(FILE_NAME):
+    print("invalid file path")
+    exit(0)
 
 def send(toSend: bytes, sock: socket):
     accepted = False
     sock.settimeout(1)
 
     while not accepted:
-        sock.sendto(toSend, ADDRESS)
+        try:
+            sock.sendto(toSend, ADDRESS)
+        except:
+            print("error sending, try again")
+            exit(0)
         try:
             data, _ = sock.recvfrom(MSS)
             # ignoring confirmations for privius packages
@@ -28,18 +41,14 @@ def send(toSend: bytes, sock: socket):
 
 
 def main():
-    # keeping track of how much we sent
-    sent = 0
-
-    # making sure the file name given exists
-    if not os.path.isfile(FILE_NAME):
-        raise ValueError('Error: Invalid file name/path')
-
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     file = open(FILE_NAME, "r")
 
+    # keeping track of how much we sent
+    sent = 0
     while chunk := bytes(file.read(MSS - 4), encoding='ascii'):
+        # adding the serial number to the start of the packege
         toSend = bytes(sent) + chunk
         send(toSend, sock)
         sent += 1
